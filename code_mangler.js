dad0b12f0e2a0333d2a2f2af5a5a5031f8e61ef9
byang123 <brianyang0721@gmail.com>
2018-12-01 12:21:57 -0500

    added a random number generator

--- code_mangler.js
+++ code_mangler.js
@@ -1,10 +1,18 @@
 function webpage_output(){
     var original_code = ""
-    var mangled_code =    
+    var mangled_code = mangle_code(original_code)
     document.write(original_code);
     document.write(mangled_code);
 }
 
 function mangle_code(code) {
-    var code_array = code.
+    var code_array = code.split("\n")
+    for (code_string in code_array) {
+        code_string.trim()
+    }
+    
 }
+
+function generateRandomNumber(min_value , max_value) {
+    return Math.random() * (max-min) + min ;
+} 
