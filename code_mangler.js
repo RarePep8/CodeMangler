14f655d40121f4f73e6a1b0390b5bbfe94c22441
byang123 <brianyang0721@gmail.com>
2018-12-01 12:03:37 -0500

    added 2 functions in code mangler

---
+++ code_mangler.js
@@ -0,0 +1,10 @@
+function webpage_output(){
+    var original_code = ""
+    var mangled_code =    
+    document.write(original_code);
+    document.write(mangled_code);
+}
+
+function mangle_code(code) {
+    var code_array = code.
+}
