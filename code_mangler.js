857b7aae1ba7132ae9e86fb1baa61102989554d9
byang123 <brianyang0721@gmail.com>
2018-12-01 15:10:04 -0500

    moved the <p> tags in index.html above the script tag so text can be outputted, also the random generator function now works and properly outputs text

--- code_mangler.js
+++ code_mangler.js
@@ -1,18 +1,37 @@
-function webpage_output(){
-    var original_code = ""
-    var mangled_code = mangle_code(original_code)
-    document.write(original_code);
-    document.write(mangled_code);
-}
-
 function mangle_code(code) {
-    var code_array = code.split("\n")
+    var code_array = code.split("\n");
     for (code_string in code_array) {
-        code_string.trim()
+        code_string.trim();
     }
-    
+    for (i = 0; i < code_array.length; i++) {
+        swap_to = generateRandomNumber(i, code_array.length-1);
+        temp_var = code_array[i];
+        code_array[i] = code_array[swap_to];
+        code_array[swap_to] = temp_var;
+        document.write(code_array[i]);
+        document.write("<br>");
+    }
 }
 
-function generateRandomNumber(min_value , max_value) {
-    return Math.random() * (max-min) + min ;
-} 
+function generateRandomNumber(min, max) {
+    return Math.round(Math.random() * (max-min) + min);
+};
+
+var original_code = "function mangle_code(code) {\n    var code_array = code.split(\"\\n\");\n    for (code_string in code_array) {\n        code_string.trim();\n    }\n    for (i = 0; i < code_array.length; i++) {\n        swap_to = generateRandomNumber(i, code_array.length-1);\n        temp_var = code_array[i];\n        code_array[i] = code_array[swap_to];\n        code_array[swap_to] = temp_var;\n    }\n}";
+"\
+function mangle_code(code) {\
+    var code_array = code.split(\"\\n\");\
+        for (code_string in code_array) {\
+            code_string.trim();\
+        }\
+        for (i = 0; i < code_array.length; i++) {\
+            swap_to = generateRandomNumber(i, code_array.length-1);\
+            temp_var = code_array[i];\
+            code_array[i] = code_array[swap_to];\
+            code_array[swap_to] = temp_var;\
+        }\
+}";
+
+var mangled_code = mangle_code(original_code);
+document.getElementById("original code").innerHTML = original_code;
+document.getElementById("mangled code").innerHTML = mangled_code;
