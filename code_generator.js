d2e5f6a7de9a7c32c5ae79749764e269aecd2965
RarePep8 <alex.wong.utsc@gmail.com>
2018-12-01 16:53:13 -0500

    Added code block generating

--- code_generator.js
+++ code_generator.js
@@ -1,24 +1,40 @@
+var vars = {"u":0, "v":0, "w":0, "x":0, "y":0, "z":0};
+var varNames = Object.keys(vars);
 function createCodeBlocks(numOfBlocks){
-    var testFunc = function(){
-        alert("1");
-        return 1;
+    // var testFunc = function(){
+    //     document.getElementById("test").innerHTML = Date();
+    // }
+    // var listOfCodeBlocks = [{text:"lmao?", function : function(){testFunc()}}];
+    var listOfCodeBlocks = [];
+    for(var i = 0; i < numOfBlocks; i++){
+        var randomVar = varNames[Math.floor((Math.random() * varNames.length))];
+        var randomValue = Math.floor((Math.random() * 4)+1);
+        listOfCodeBlocks.push(createArithmeticBlock(randomVar, randomValue));
     }
-    var listOfCodeBlocks = [testFunc()];
-
-
-
-    return listOfCodeBlocks
+    var allText = "";
+    for(var i = 0 ; i < listOfCodeBlocks.length; i++){
+        allText += listOfCodeBlocks[i].text +"\n";
+    }
+    document.getElementById("test").innerHTML = allText;
+    return listOfCodeBlocks;
 }
 
 
+function createArithmeticBlock(varToChangeName, valueName){
+    var createdBlock = function(varToChange, value){
+        if(isNumeric(value)){
+            value = parseInt(value);
+        }
+        return varToChange += value;
+    };
+    var createdBlockName = varToChangeName + " += " + valueName + ";";
 
-
-
-function createAssignBlock(assignTo, assignFrom){
-    assignTo = assign;
+    return {"text":createdBlockName, "function":function(varToChange,value){return createdBlock(varToChange,value);}, "params":[varToChangeName, valueName]};
 }
-var currFunc = function(numOfBlocks){
 
-};
-createCodeBlocks(10);
-document.getElementById("test").innerHTML = "Hello JavaScript!";
+var list = createCodeBlocks(10);
+
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
