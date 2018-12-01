fd6010e1488dfed4550a2fb10f7c126b2209c2ea
RarePep8 <alex.wong.utsc@gmail.com>
2018-12-01 18:28:13 -0500

    Added more code block variations

--- code_generator.js
+++ code_generator.js
@@ -1,4 +1,4 @@
-var vars = {"u":0, "v":0, "w":0, "x":0, "y":0, "z":0};
+var vars = {"x":0, "y":0, "z":0};
 var varNames = Object.keys(vars);
 function createCodeBlocks(numOfBlocks){
     // var testFunc = function(){
@@ -7,9 +7,18 @@ function createCodeBlocks(numOfBlocks){
     // var listOfCodeBlocks = [{text:"lmao?", function : function(){testFunc()}}];
     var listOfCodeBlocks = [];
     for(var i = 0; i < numOfBlocks; i++){
+
         var randomVar = varNames[Math.floor((Math.random() * varNames.length))];
         var randomValue = Math.floor((Math.random() * 4)+1);
-        listOfCodeBlocks.push(createArithmeticBlock(randomVar, randomValue));
+        var randomBlockNum = Math.random();
+        if(randomBlockNum < 0.5){
+            listOfCodeBlocks.push(createAssignBlock(randomVar, randomValue));
+        } else if (randomBlockNum < 0.75){
+            listOfCodeBlocks.push(createArithmeticBlock(randomVar, randomValue));
+        } else{
+            listOfCodeBlocks.push(createLoopBlock(randomVar, randomValue, Math.floor((Math.random() * 4)+1)));
+        }
+
     }
     var allText = "";
     for(var i = 0 ; i < listOfCodeBlocks.length; i++){
@@ -29,7 +38,7 @@ function createCodeBlocks(numOfBlocks){
         }else{
             addValue = params[1];
         }
-        vars[params[0]] = currFunc(vars[params[0]], addValue);
+        vars[listOfCodeBlocks[i].output] = currFunc(vars[params[0]], addValue);
     }
     output = vars["x"];
     return {"text":allText, "samples":[{"input":input, "output":output}]};
@@ -38,16 +47,32 @@ function createCodeBlocks(numOfBlocks){
 
 function createArithmeticBlock(varToChangeName, valueName){
     var createdBlock = function(varToChange, value){
-        if(isNumeric(value)){
-            value = parseInt(value);
-        }
         return varToChange += value;
     };
     var createdBlockName = varToChangeName + " += " + valueName + ";";
 
-    return {"text":createdBlockName, "function":function(varToChange,value){return createdBlock(varToChange,value);}, "params":[varToChangeName, valueName]};
+    return {"text":createdBlockName, "function":function(varToChange,value){return createdBlock(varToChange,value);}, "params":[varToChangeName, valueName], "output": varToChangeName};
 }
 
+function createAssignBlock(varToChangeName, valueName){
+    var createdBlock = function(varToChange, value){
+        return varToChange = value;
+    };
+    var createdBlockName = varToChangeName + " = " + valueName + ";";
+
+    return {"text":createdBlockName, "function":function(varToChange,value){return createdBlock(varToChange,value);}, "params":[varToChangeName, valueName], "output": varToChangeName};
+}
+
+function createLoopBlock(varToChangeName, valueName, incrementCount){
+    var createdBlock = function(varToChange, value){
+        for(var i = 0; i < value; i++ ){
+            varToChange += incrementCount;
+        }
+        return varToChange;
+    }
+    var createdBlockName = "for(int i=0; i < " + valueName + "; i++){ " + varToChangeName + "+=" + incrementCount + " ;}";
+    return {"text":createdBlockName, "function":function(varToChange,value){return createdBlock(varToChange,value);}, "params":[varToChangeName, valueName], "output": varToChangeName};
+}
 
 var list = createCodeBlocks(10);
 
