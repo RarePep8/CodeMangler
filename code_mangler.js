6750d817685974813f676666fb4e26b0ff35b5ab
byang123 <brianyang0721@gmail.com>
2018-12-01 16:51:30 -0500

    added the changes to css to accomodate the code blocks

--- code_mangler.js
+++ code_mangler.js
@@ -8,8 +8,8 @@ function mangle_code(code) {
         temp_var = code_array[i];
         code_array[i] = code_array[swap_to];
         code_array[swap_to] = temp_var;
-        document.getElementById("mangled code").innerHTML += '<li class="column" draggable="true"><header>' + code_array[i] + '<//header></li>';
-        document.getElementById("mangled code").innerHTML += ("<br>");
+        document.getElementById("mangled code").innerHTML += '<li class="code" draggable="true"><header>' + code_array[i] + '<//header></li>';
+        //document.getElementById("mangled code").innerHTML += ("<br>");
     }
 }
 
