670bfd9fa45976e92095c80891105ba06f9c930d
RarePep8 <alex.wong.utsc@gmail.com>
2018-12-01 12:03:58 -0500

    Added test paragraph

---
+++ code_generator.js
@@ -0,0 +1,24 @@
+function createCodeBlocks(numOfBlocks){
+    var testFunc = function(){
+        alert("1");
+        return 1;
+    }
+    var listOfCodeBlocks = [testFunc()];
+
+
+
+    return listOfCodeBlocks
+}
+
+
+
+
+
+function createAssignBlock(assignTo, assignFrom){
+    assignTo = assign;
+}
+var currFunc = function(numOfBlocks){
+
+};
+createCodeBlocks(10);
+document.getElementById("test").innerHTML = "Hello JavaScript!";
