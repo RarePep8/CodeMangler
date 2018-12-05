4d128060281e556479c7a02d8d54fd3d4c794294
rarepep8 <alex.wong.utsc@gmail.com>
2018-12-04 22:19:01 -0500

    Remade code generator

--- code_generator.js
+++ code_generator.js
@@ -1,83 +1,90 @@
-var vars = {"x":0, "y":0, "z":0};
-var varNames = Object.keys(vars);
-function createCodeBlocks(numOfBlocks){
-    // var testFunc = function(){
-    //     document.getElementById("test").innerHTML = Date();
-    // }
-    // var listOfCodeBlocks = [{text:"lmao?", function : function(){testFunc()}}];
-    var listOfCodeBlocks = [];
-    for(var i = 0; i < numOfBlocks; i++){
+var varObj = {"X":0, "Y":0, "Z":1};
+var list = Object.keys(varObj);
 
-        var randomVar = varNames[Math.floor((Math.random() * varNames.length))];
-        var randomValue = Math.floor((Math.random() * 4)+1);
-        var randomBlockNum = Math.random();
-        if(randomBlockNum < 0.5){
-            listOfCodeBlocks.push(createAssignBlock(randomVar, randomValue));
-        } else if (randomBlockNum < 0.75){
-            listOfCodeBlocks.push(createArithmeticBlock(randomVar, randomValue));
-        } else{
-            var randomValue = varNames[Math.floor((Math.random() * varNames.length))];
-            listOfCodeBlocks.push(createLoopBlock(randomVar, randomValue, Math.floor((Math.random() * 4)+1)));
+class CodeBlock{
+    constructor(functionRef, params, string){
+        this.functionRef = functionRef;
+        this.params = params;
+        this.string = string;
+    }
+    runCode(varObj){
+        var paramList = [].concat([varObj], this.params);
+        try{
+            this.functionRef.apply(null, paramList);
         }
-
-    }
-    var allText = "";
-    for(var i = 0 ; i < listOfCodeBlocks.length; i++){
-        allText += listOfCodeBlocks[i].text +"\n";
-    }
-    document.getElementById("test").innerHTML = allText;
-
-    var input = 1;
-    var vars = {"u":0, "v":0, "w":0, "x":0, "y":0, "z":0};
-    vars["x"] = input;
-    for(var i = 0; i < listOfCodeBlocks.length; i++){
-        var currFunc = listOfCodeBlocks[i].function;
-        var params = listOfCodeBlocks[i].params;
-        var addValue = 0;
-        if(params[1] in vars){
-            addValue = vars[params[1]];
-        }else{
-            addValue = params[1];
+        catch(err){
+            alert("Error: something went wrong when running the function " + this.functionRef.string);
         }
-        vars[listOfCodeBlocks[i].output] = currFunc(vars[params[0]], addValue);
     }
-    output = vars["x"];
-    return {"text":allText, "samples":[{"input":input, "output":output}]};
 }
-
-
-function createArithmeticBlock(varToChangeName, valueName){
-    var createdBlock = function(varToChange, value){
-        return varToChange += value;
-    };
-    var createdBlockName = varToChangeName + " += " + valueName + ";";
-
-    return {"text":createdBlockName, "function":function(varToChange,value){return createdBlock(varToChange,value);}, "params":[varToChangeName, valueName], "output": varToChangeName};
-}
-
-function createAssignBlock(varToChangeName, valueName){
-    var createdBlock = function(varToChange, value){
-        return varToChange = value;
-    };
-    var createdBlockName = varToChangeName + " = " + valueName + ";";
-
-    return {"text":createdBlockName, "function":function(varToChange,value){return createdBlock(varToChange,value);}, "params":[varToChangeName, valueName], "output": varToChangeName};
-}
-
-function createLoopBlock(varToChangeName, valueName, incrementCount){
-    var createdBlock = function(varToChange, value){
-        for(var i = 0; i < value; i++ ){
-            varToChange += incrementCount;
+class Program{
+    constructor(){
+        this.codeBlockList = [];
+    }
+    addBlocks(numOfBlocks){
+        for(var i = 0 ; i < numOfBlocks ; i++){
+            this.codeBlockList.push(createRandomBlock(list));
         }
-        return varToChange;
     }
-    var createdBlockName = "for(var i=0; i < " + valueName + "; i++){ " + varToChangeName + "+=" + incrementCount + " ;}";
-    return {"text":createdBlockName, "function":function(varToChange,value){return createdBlock(varToChange,value);}, "params":[varToChangeName, valueName], "output": varToChangeName};
+    runAllCode(varObj){
+        for(var i = 0 ; i < this.codeBlockList.length; i++){
+            this.codeBlockList[i].runCode(varObj);
+        }
+    }
+}
+function createRandomBlock(listOfVars){
+    var randNum = Math.random();
+    if(randNum < 0.5){
+        return createAssignBlock(listOfVars);
+    } else {
+        return createArithmeticBlock(listOfVars);
+    }
+}
+function createAssignBlock(listOfVars){
+    // Pick two random variables for assign statement
+    var pickResult = pickNonRepeatVar(listOfVars);
+    var assignedTo = pickResult.pick;
+    listOfVars = pickResult.remaining;
+    pickResult = pickNonRepeatVar(listOfVars);
+    var assignedFrom = pickResult.pick;
+    // Create function reference that takes in an object with all mutable variables, and 2 variables to mutate
+    var createdFunction = function(varObj, assignedToName, assignedFromName){
+        varObj[assignedToName] = varObj[assignedFromName];
+    } 
+    var codeBlock = new CodeBlock(functionRef = createdFunction, params = [assignedTo, assignedFrom], string = assignedTo + " = " + assignedFrom + ";");
+    return codeBlock;
 }
 
-var list = createCodeBlocks(10);
-
-
-function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+function createArithmeticBlock(listOfVars){
+    var pickResult = pickNonRepeatVar(listOfVars);
+    var addedTo = pickResult.pick;
+    listOfVars = pickResult.remaining;
+    if(Math.random < 0.5){
+        pickResult = pickNonRepeatVar(listOfVars);
+        var addedFrom = pickResult.pick;
+    } else{
+        var addedFrom = Math.round(Math.random()*5);
+    }
+    var createdFunction = function(varObj, addedToName, addedFromName){
+        var parsedInt = parseInt(addedFromName);
+        if(isNaN(parsedInt)){
+            varObj[addedToName] += varObj[addedFromName];
+        } else {
+            varObj[addedToName] += parsedInt;
+        }
+    } 
+    var codeBlock = new CodeBlock(functionRef = createdFunction, params = [addedTo, addedFrom], string = addedTo + " += " + addedFrom + ";");
+    return codeBlock;
 }
+
+function pickNonRepeatVar(listOfElements){
+    var randI = Math.round(Math.random()*(listOfElements.length-1));
+    return {"pick": listOfElements[randI], "remaining": [].concat(listOfElements.slice(0, randI), listOfElements.slice(randI+1))};
+}
+var p = new Program();
+p.addBlocks(10)
+p.runAllCode(varObj);
+alert(varObj.X);
+// for(var i = 0; i < p.codeBlockList.length; i++){
+//     alert(p.codeBlockList[i].string);
+// }
